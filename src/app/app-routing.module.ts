673f405069b27b026a2a657a2aa1aae8f5abe44e
elisachris1 <elisachris1@gmail.com>
2022-08-27 16:04:20 -0700

    technician route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 
 const routes: Routes = [
   {
     path: '', 
     component: NavComponent, children: [
       {
-        path: 'home', component: HomeComponent
-      }
+        path: 'home', component: HomeComponent},
+        {path: 'technicians', component: TechnicianListComponent}
     ]
   }
 ];
