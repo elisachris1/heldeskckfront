558a5e59431f7fa52903e2fdc03661cf9a2fcfbc
elisachris1 <elisachris1@gmail.com>
2022-08-27 17:06:29 -0700

    login CSS and HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {
-    path: '', 
-    component: NavComponent, children: [
-      {
-        path: 'home', component: HomeComponent},
-        {path: 'technicians', component: TechnicianListComponent}
+    path: '', component: NavComponent, children:[
+      {path:'home', component: HomeComponent},
+      {path: 'technicians', component: TechnicianListComponent},
+        
     ]
   }
 ];
